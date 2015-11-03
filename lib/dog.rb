da4f2d7bc58599dd4f96032c39be5f9b29d69924
brydela <akestrelgrey@gmail.com>
2015-11-03 04:31:24 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -15,21 +15,3 @@ class Dog
       @breed = new_breed
   end
 end
-
-class Person
-  def name
-    @name
-  end
-  
-  def job
-    @job
-  end
-  
-  def name=(new_name)
-    @name = new_name
-  end
-  
-  def job=(new_job)
-    @job = new_job
-  end
-end
