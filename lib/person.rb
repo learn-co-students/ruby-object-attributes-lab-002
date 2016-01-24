508c400fe8938e931e31fffef9a9bcd9f0aa9b3f
deakattila <deakattila87@gmail.com>
2016-01-24 16:29:50 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,18 @@
+class Person
+
+  def name=(personname)
+    @name = personname
+  end
+
+  def name
+    @name
+  end
+
+  def job=(personjob)
+    @job = personjob
+  end
+
+  def job
+    @job
+  end
+end
